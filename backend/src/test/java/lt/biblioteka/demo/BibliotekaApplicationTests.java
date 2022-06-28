7b799ccf42ef762196ce1337ac4653ac4ec67fb9
BroniusK <kazlauskas.bro@gmail.com>
2022-06-28 16:55:13 +0300

    pridetas backas

---
+++ backend/src/test/java/lt/biblioteka/demo/BibliotekaApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.biblioteka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
